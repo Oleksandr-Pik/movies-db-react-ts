922bfe4e36108ccca7322259743603dcb8deb8da
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-06-03 23:54:57 +0300

    add getNowPlaying

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -4,6 +4,28 @@ import { RootState } from "../../store";
 import MovieCard from "./MovieCard";
 
 import styles from "./Movies.module.scss";
+import { useState } from "react";
+
+async function getNowPlaying() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2RhM2MyMjFlZjA3NDBhMDFlNmMwZTllNzdkNzJkMCIsInN1YiI6IjY1OTU0Y2UwYTY5OGNmNWExMzQzYTA1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KQiqVQlKVKjNnvB8aixow-b7QW092Q8cG4KYkgdhwxo'
+    }
+  };
+
+  const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  const json = response.json()
+
+  return json;
+}
+
+function MoviesFetch() {
+  const [movies, setMovies] = useState([]);
+  
+  return <Movies movies={movies} />;
+}
 
 interface MoviesProps {
   movies: Movie[]
